af46ba7e51b98130d412ec9ac52d55f6e83375d2
Andreas Nordahl <andnorda@gmail.com>
2021-02-26 10:45:10 +0100

    remove nextjs example project for clarity

--- examples/nextjs/pages/_app.js
+++
@@ -1,8 +0,0 @@
-import "../styles/globals.css";
-import "nav-frontend-paneler-style/dist/main.css";
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
