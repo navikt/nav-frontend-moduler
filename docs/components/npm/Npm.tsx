a56766a7024c1fd1b3ca191df923fe2ece42be9b
Ken <ken.aleksander@gmail.com>
2021-03-12 15:33:15 +0100

    oppdatert npm module

--- docs/components/npm/Npm.tsx
+++ docs/components/npm/Npm.tsx
@@ -2,6 +2,8 @@ import getConfig from "next/config";
 import { Link } from "@navikt/ds-react";
 import { ExternalLink } from "@navikt/ds-icons";
 import Example from "../example/Example";
+import Bash from "../code/Bash";
+import style from "./npm.module.css";
 
 /* interface Props {} */
 
@@ -15,21 +17,58 @@ const Npm = ({ name }: { name: string }) => {
     <div>
       {packs.map(({ name, data }) => (
         <>
-          <Link
+          {/* <Link
             style={{ columnGap: "0.5rem" }}
             href={`https://www.npmjs.com/package/${name}`}
             target="_blank"
           >
             <ExternalLink style={{ fontSize: "1.5rem" }} />
             {"NPM-pakke v" + data.version}
-          </Link>
-          <Example
+          </Link> */}
+          <span className={style.badges}>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href={`https://www.npmjs.com/package/${name}`}
+            >
+              <img src={`https://badgen.net/npm/v/${name}`} alt="123" />
+            </a>
+
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href={`https://bundlephobia.com/result?p=${name}`}
+            >
+              <img
+                src={`https://badgen.net/bundlephobia/tree-shaking/${name}`}
+                alt="123"
+              />
+            </a>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href={`https://bundlephobia.com/result?p=${name}`}
+            >
+              <img
+                src={`https://badgen.net/bundlephobia/min/${name}`}
+                alt="123"
+              />
+            </a>
+          </span>
+          <Bash code={`import { Button } from "${name}"`} language="jsx" copy />
+          <Bash
+            code={`<link src="https://unpkg.com/${name}@${data.version}/" />`}
+            language="html"
+            copy
+          />
+
+          {/* <Example
             text="import { Button } from '@navikt/ds-react'"
             html={
               name.includes("@navikt/ds-") &&
               `<link src="https://unpkg.com/${name}@${data.version}/" />`
             }
-          />
+          /> */}
         </>
       ))}
     </div>
